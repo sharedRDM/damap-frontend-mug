2a8d36bb0bd0b4647ccfdf93a634ea165f06b85e
Laura <lthaci@tugraz.at>
2026-04-29 15:49:04 +0200

    update: i18n changes and admin banner

--- libs/damap/src/lib/widgets/app-banner/app-banner.module.ts
+++ libs/damap/src/lib/widgets/app-banner/app-banner.module.ts
@@ -1,8 +1,12 @@
 import { AppBannerComponent } from './app-banner.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppBannerComponent],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   exports: [AppBannerComponent],
 })
 export class AppBannerModule {}
